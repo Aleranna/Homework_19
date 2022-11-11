0b657b39dbc930f10dc9b8e907a97a3b2cb9d446
Aleranna <aleranna22@gmail.com>
2022-11-11 13:58:23 +0300

    cc

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restx import Api
 
 from config import Config
@@ -21,7 +21,8 @@ def register_extensions(app):
     db.init_app(app)
     with app.app_context():
         db.create_all()
-    api = Api(app)
+    api = Api(app, doc='/doc')
+    #api.init_app(app)
     api.add_namespace(director_ns)
     api.add_namespace(genre_ns)
     api.add_namespace(movie_ns)
@@ -29,8 +30,14 @@ def register_extensions(app):
     api.add_namespace(auth_ns)
 
 
+
 app = create_app(Config())
 app.debug = True
+
+@app.route('/')
+def index_page():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
